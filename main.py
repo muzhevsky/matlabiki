6f3f51519fb750949e03087dbd07f574ee01124d
inizioIT <garanin203@yandex.ru>
2024-10-23 10:32:57 +0400

    Pasha 5

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import uvicorn
 from flask import Flask, request
+from flask_cors import CORS
 from calc_and_draw import CalculationDrawService
 
 app = Flask(__name__)
+CORS(app, origins="*")
 calcS = CalculationDrawService()
 
 
@@ -13,18 +15,17 @@ def check():
 
 @app.route('/calcAndDraw', methods=['POST'])
 def calc_and_draw():
-    if request.method == 'POST':
-        #  Получение данных с фронт энда
-        content = request.json
-        X, Y = calcS.calculate(content)
-        # Отрисовка графиков
-        path1 = calcS.save_plot(X, Y)
-        path2 = calcS.save_petal_plots(0, Y, content["max_values"])
+    #  Получение данных с фронт энда
+    content = request.json
+    X, Y = calcS.calculate(content)
+    # Отрисовка графиков
+    path1 = calcS.save_plot(X, Y)
+    path2 = calcS.save_petal_plots(0, Y, content["max_values"])
 
-        return {
-            "image1": path1,
-            "image2": path2
-        }
+    return {
+        "image1": path1,
+        "image2": path2
+    }
 
 
 if __name__ == '__main__':
