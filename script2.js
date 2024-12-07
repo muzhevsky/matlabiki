29c39a912b909c5773cf57221f03936a272de48b
Andrew <andreimuzhevsky@gmail.com>
2024-12-07 15:36:11 +0400

    lab2 refactored

--- script2.js
+++ script2.js
@@ -1,9 +1,53 @@
 window.addEventListener("DOMContentLoaded", function () {
-    let funcBlock = document.getElementById("leftBlock")
-    let varBlock = document.getElementById("rightBlock")
-    let controlBlock = document.getElementById("controlBlock")
-    let funcNumber = 76;
-    let shitMap = new Map([["var", "start"], ["limit", "max_values"], ["k", "k"], ["b", "b"]]);
+    let container = document.getElementById("main")
+    let saveToJsonButton = document.getElementById("saveJsonButton")
+    let loadToJsonButton = document.getElementById("loadJsonButton")
+    let postDataButton = document.getElementById("postDataButton")
+    let varBlock = createVarBlock();
+    let funcBlock = createFuncBlock();
+    let eFuncBlock = createEFuncBlock();
+
+    let jsonData = localStorage.getItem("jsonData2")
+    if (jsonData) {
+        data = convertJsonToData(jsonData)
+        varBlock.setData(data)
+        funcBlock.setData(data)
+        eFuncBlock.setData(data)
+    }
+
+    container.append(varBlock.block, funcBlock.block, eFuncBlock.block)
+
+    saveToJsonButton.onclick = () => {
+        saveJSONToFile(convertDataToJson(varBlock, funcBlock, eFuncBlock))
+    }
+
+    loadToJsonButton.onclick = () => {
+        loadJSONFromFile()
+            .then((result) => {
+                let data = convertJsonToData(result)
+                varBlock.setData(data)
+                funcBlock.setData(data)
+                eFuncBlock.setData(data)
+            })
+            .catch((e) => console.log(e))
+    }
+
+    postDataButton.onclick = () => {
+        postDataButton.disabled = true
+        let body = convertDataToJson(varBlock, funcBlock, eFuncBlock)
+        localStorage.setItem("jsonData2", body)
+        postData("http://localhost:9090/calcAndDraw_car", body,
+            () => {
+                document.querySelectorAll('#imageGallery .plot').forEach(img => {
+                    img.src += '?t=' + new Date().getTime();
+                });
+            },
+            () => { },
+            () => postDataButton.disabled = false)
+    }
+});
+
+function createVarBlock() {
     let varNames = [
         "Общее количество ДТП в РФ (в год)",
         "Количество погибших в ДТП",
@@ -20,11 +64,127 @@ window.addEventListener("DOMContentLoaded", function () {
         "Количество ДТП с тяжкими последствиями"
     ]
 
-    funcBlock.append(createFuncBlock(funcNumber))
-    varBlock.append(createConstTable(["название переменной", "значение", "лимит"], varNames, ["var", "limit"]))
-    varBlock.append(createConstTable(["функция", "k[i]", "b[i]"], ["E1", "E2", "E3", "E4", "E5", "E6"], ["k", "b"]))
-    controlBlock.append(createControlBlock("http://194.147.149.181:9090/calcAndDraw_car", shitMap, funcNumber))
-    // controlBlock.append(createControlBlock("http://localhost:9090/calcAndDraw_car", shitMap, funcNumber))
+    let rowNameFunc = function (i) {
+        if (i >= varNames.length) {
+            return ""
+        }
 
-    fillInputsWithValues()
-});
+        return varNames[i];
+    }
+
+    let cellNameFunc = function (i, j) {
+        if (i >= varNames.length) {
+            return "";
+        }
+
+        return " ";
+    }
+
+    return NewBlock(["Название переменной", "Значение", "Лимит"], "var", rowNameFunc, cellNameFunc)
+}
+
+function createFuncBlock() {
+    let flArray = new Array();
+    flArray.push(7, 8, 9, 10, 11, 12, 4, 5, 8, 9, 11, 6, 7, 8, 9, 10, 11, 12, 13, 2, 5, 7, 8, 9, 11, 2, 4, 7, 8, 11, 3, 7, 8, 9, 10, 11, 13, 1, 8, 13, 2, 5, 10, 1, 7, 9, 11, 12, 1, 10, 11, 12, 2, 3, 1, 7, 8, 9, 13, 2, 3, 1, 8, 12, 1, 7, 8, 11, 1, 2, 3, 4, 5, 6, 7, 8);
+    power = 3;
+
+    rowNameFunc = function (i) {
+        if (i >= flArray.length) {
+            return "";
+        }
+        return RowNameWithIndex("F", i + 1)
+    }
+
+    cellNameFunc = function (i, j) {
+        if (i >= flArray.length) {
+            return "";
+        }
+        return PolynomicCellName("L", flArray[i], "(t)", power - j)
+    }
+
+    return NewBlock(["Функции", "", "", "", ""], "x", rowNameFunc, cellNameFunc)
+}
+
+function createEFuncBlock() {
+    let varNames = [
+        "E1(t) - Количество транспортных средств старше 10 лет",
+        "E2(t) – Степень износа транспортных средств",
+        "E3(t) – Количество комплексов фото-видео фиксации нарушений",
+        "E4(t) – Количество выданных водительских удостоверений",
+        "E5(t) – Протяженность участков дорог",
+        "E6(t) – Средний штраф за нарушение ПДД",
+    ]
+
+    let rowNameFunc = function (i) {
+        if (i >= varNames.length) {
+            return ""
+        }
+
+        return varNames[i];
+    }
+
+    let cellNameFunc = function (i, j) {
+        if (i >= varNames.length) {
+            return "";
+        }
+
+        if (j == 0) { return NameWithIndex("k", i + 1) }
+        if (j == 1) { return NameWithIndex("b", i + 1) }
+    }
+
+    return NewBlock(["Функция", "", ""], "efunc", rowNameFunc, cellNameFunc)
+}
+
+// todo можно вынести в либу и переиспользовать, но так лень пипец
+function convertDataToJson(varBlock, funcBlock, eFuncBlock) {
+    let result = {
+        start: new Array(0),
+        max_values: new Array(0),
+        k: new Array(0),
+        b: new Array(0),
+        coef: new Array(0)
+    }
+
+    var data = varBlock.getData()
+    for (let i = 0; i < data.length; i++) {
+        result.start.push(data[i][0])
+        result.max_values.push(data[i][1])
+    }
+
+    data = funcBlock.getData();
+    for (let i = 0; i < data.length; i++) {
+        result.coef[i] = data[i]
+    }
+
+    data = eFuncBlock.getData()
+    for (let i = 0; i < data.length; i++) {
+        result.k.push(data[i][0])
+        result.b.push(data[i][1])
+    }
+
+    return JSON.stringify(result)
+}
+
+function convertJsonToData(jsonString) {
+    let data = {
+        var: new Array(),
+        x: new Array(),
+        efunc: new Array()
+    }
+
+    let parsedJson = JSON.parse(jsonString)
+    for (let i = 0; i < parsedJson.start.length; i++) {
+        data.var.push([parsedJson.start[i], parsedJson.max_values[i]])
+    }
+
+    for (let i = 0; i < parsedJson.coef.length; i++) {
+        data.x.push(parsedJson.coef[i])
+    }
+
+    for (let i = 0; i < parsedJson.k.length; i++) {
+        data.efunc.push([parsedJson.k[i], parsedJson.b[i]])
+    }
+
+    return data
+}
+
